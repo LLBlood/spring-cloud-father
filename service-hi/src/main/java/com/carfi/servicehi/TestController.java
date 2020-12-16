27e654eda8ec3959c1f296ae3b4fafec8f88ff71
liulin <liulin@gmail.com>
2020-12-16 16:22:58 +0800

    story#create spring cloud and more maven

---
+++ service-hi/src/main/java/com/carfi/servicehi/TestController.java
@@ -0,0 +1,25 @@
+package com.carfi.servicehi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * com.carfi.servicehi$
+ *
+ * @author ll
+ * @date 2020-12-16 16:13:03
+ **/
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "carfi") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+}
