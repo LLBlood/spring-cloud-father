069abba62e3e35eac4eae3b3ed64e6ae9c778d0f
liulin <liulin@gmail.com>
2020-12-18 10:22:50 +0800

    重新构造所有POM文件，保证使用maven多模块

--- service-feign/src/main/java/com/carfi/servicefeign/service/SchedualServiceHi.java
+++ service-feign/src/main/java/com/carfi/servicefeign/service/SchedualServiceHi.java
@@ -1,7 +1,7 @@
 package com.carfi.servicefeign.service;
 
 import com.carfi.servicefeign.fallback.SchedualServiceHiHystric;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
