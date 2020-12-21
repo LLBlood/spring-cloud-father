65feac3d365982f782be64e8cc481e14e1bbfce3
liulin <liulin@gmail.com>
2020-12-21 14:31:43 +0800

    断路器监控(Hystrix Dashboard)

--- service-hi/src/main/java/com/carfi/servicehi/TestController.java
+++ service-hi/src/main/java/com/carfi/servicehi/TestController.java
@@ -1,5 +1,6 @@
 package com.carfi.servicehi;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,12 @@ public class TestController {
     private String port;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "name", defaultValue = "carfi") String name) {
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
