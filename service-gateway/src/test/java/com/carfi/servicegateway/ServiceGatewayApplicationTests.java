8e72bbe37d0305c9cdd5284b959024f167ce98fc
liulin <liulin@gmail.com>
2020-12-22 11:29:05 +0800

    story# 增加网关

---
+++ service-gateway/src/test/java/com/carfi/servicegateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carfi.servicegateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
